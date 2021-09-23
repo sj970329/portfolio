b0bc6cda3f580318cebb759460cfeedaf0888d04
sj970329 <xox_s2@naver.com>
2021-09-23 11:31:51 +0900

    edit
    ing

---
+++ js/script.js
@@ -0,0 +1,73 @@
+var targetMenu = document.querySelector(".targetMenu");
+var onTaget = document.querySelectorAll(".targetMenu ul li");
+var section = document.querySelectorAll("section");
+var sectionCounter = 0; 
+var offset = 0;
+var count=0;
+document.addEventListener('wheel', scrollAni);
+window.addEventListener('load', activeMenu);
+targetMenu.addEventListener('click', gosection);
+
+window.onload = function() {
+
+function onClick() {
+    document.querySelector('.modal_wrap').style.display ='block';
+    document.querySelector('.black_bg').style.display ='block';
+    }   
+    function offClick() {
+     document.querySelector('.modal_wrap').style.display ='none';
+     document.querySelector('.black_bg').style.display ='none';
+    }
+
+    document.getElementById('modal_btn').addEventListener('click', onClick);
+    document.querySelector('.modal_close').addEventListener('click', offClick);
+
+};
+
+
+function scrollAni(e){
+    count++;
+    var gosectioncount = count%5;
+    if(e.wheelDelta<0){
+        if(gosectioncount == 4 && sectionCounter<5)sectionCounter++;
+    }else{
+        if(gosectioncount == 4 && sectionCounter>0)sectionCounter--;
+    }
+    
+    offset = innerHeight*sectionCounter;
+
+    $("html, body").stop().animate({scrollTop:offset},600,"easeInOutExpo");
+
+    setTimeout(activeMenu,300);
+    onTargetMenu();
+}
+
+function activeMenu(){
+    section.forEach(function(ele){ele.classList.remove("active");});
+    section[sectionCounter].classList.add("active");
+}
+
+function gosection(e){
+    sectionCounter = Number(e.target.getAttribute("datanum"));
+    offset = innerHeight*sectionCounter;
+    $("html, body").stop().animate({scrollTop:offset},600,"easeInOutExpo");
+    setTimeout(activeMenu,300);
+    onTargetMenu();
+}
+
+function onTargetMenu(){
+    onTaget.forEach(function(menu){
+        menu.children[0].classList.remove("on");
+    });
+    onTaget[sectionCounter].children[0].classList.add("on");
+}
+
+// 스크롤 막기 시작
+$('html, body').css({'overflow': 'hidden', 'height': '100%'});
+$('#element').on('scroll touchmove mousewheel', function(event) {
+event.preventDefault();
+event.stopPropagation();
+return false;
+});
+//스크롤 막기 끝
+
